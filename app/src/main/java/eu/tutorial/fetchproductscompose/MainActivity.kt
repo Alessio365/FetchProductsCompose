89cf508caf31e4c640f169922e2f233ca29193d0
Collurinho <115481576+Collurinho@users.noreply.github.com>
2024-10-18 12:37:56 +0200

    feat: Implement MainActivity with product fetching

--- app/src/main/java/eu/tutorial/fetchproductscompose/MainActivity.kt
+++ app/src/main/java/eu/tutorial/fetchproductscompose/MainActivity.kt
@@ -3,23 +3,28 @@ package eu.tutorial.fetchproductscompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import eu.tutorial.fetchproductscompose.ui.ProductListScreen
 import eu.tutorial.fetchproductscompose.ui.theme.FetchProductsComposeTheme
+import eu.tutorial.fetchproductscompose.viewmodel.ProductViewModel
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: ProductViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // enableEdgeToEdge()
         setContent {
             FetchProductsComposeTheme {
-
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    viewModel.fetchProducts()
+                    ProductListScreen(viewModel)
+                }
             }
         }
     }
